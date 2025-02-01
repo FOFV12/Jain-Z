ff7017e4645218eef9b89e4b0e0aa149608d3679
FOFV12 <105760335+FOFV12@users.noreply.github.com>
2025-02-01 20:17:48 +0530

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 module.exports = {
 
   /**
-   * @description requires settings for the bot
+   
    */
 
-  TOKEN: process.env.TOKEN || "",  // your bot token
-  PREFIX: process.env.PREFIX || "", //<= default is #  // bot prefix
-  OWNER_ID: process.env.OWNER_ID || "", //your client id
+  TOKEN: process.env.TOKEN || "MTMzNDg4NDMzMzE5MzkyNDYzOQ.G7dZxR.WojT0pfN0OrG1eYLqoz9d7UDe2BpIGfcB-1iCM",  // your bot token
+  PREFIX: process.env.PREFIX || ".", //<= default is #  // bot prefix
+  OWNER_ID: process.env.OWNER_ID || "1057588613463953498", //your client id
 
   /**
    *
@@ -19,7 +19,7 @@ module.exports = {
   /**
    * @description boolean settings for the bot
    */
-  LEAVE_EMPTY: true, //<= default is true "true" = use and "false" = not use!  // leave voice channel when empty
-  LEAVE_FINISH: true, //<= default is true "true" = use and "false" = not use!  // leave voice channel when finished
+  LEAVE_EMPTY: false, //<= default is true "true" = use and "false" = not use!  // leave voice channel when empty
+  LEAVE_FINISH: false, //<= default is true "true" = use and "false" = not use!  // leave voice channel when finished
   LEAVE_STOP: true, //<= default is true "true" = use and "false" = not use!  // leave voice channel when stopped
 }
